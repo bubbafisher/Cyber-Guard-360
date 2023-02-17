f7404898d98187df02f7d1a49215edacd20b6bd7
pacezd <pacezd@g.cofc.edu>
2023-02-17 12:25:43 -0500

    Adding Correct Files
    these are all of  the files created for the main page

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+              rel="stylesheet"
+              integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+              crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="main.css"/>
+        <title>CyberGuard 360</title>
+        
+    </head>
+    <body>
+        <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-3">
+                        <img src='images/CG360.png' alt='Cyber Guard Logo'>
+                    </div>
+                <div class="col-9">
+                    <button>Sign In</button>
+                </div>
+                </div>
+                
+            <div class="container-fluid2">
+             <div class="row2">
+                 <div class="col-12">
+                     <nav>
+                         <ul>
+                             <li><a href="#">Products</a></li>
+                             <li><a href="#">Other</a></li>
+                         </ul>
+                     </nav>
+                 </div>
+             </div>
+                
+                </div>
+            </div>
+        </header>
+        
+        <!-- creating landing page Hero -->
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>Welcome to CyberGuard360</h1>
+                <p>Test Your Business' PCI Compliance With Us </p>
+                
+                <button>Secure Now!</button>
+            </div>
+            
+            
+        </div>
+        
+    </body>
+</html>
---
+++ views/create_account.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Create Account</title>
+    </head>
+    
+    <body>
+        <main>
+            <h1>Create Account</h1>
+        </main>
+    </body>
+</html>>
