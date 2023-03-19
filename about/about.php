872688988fe88a6045f1586024bbde26cd267e32
AwesomenessPrimal <brendanfisher88@gmail.com>
2023-03-19 09:36:12 -0400

    Added about page with useful links

---
+++ about/about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+              rel="stylesheet"
+              integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+              crossorigin="anonymous">
+        
+        
+        <link rel="stylesheet" href="../main.css"/>
+        <title>CyberGuard 360</title>
+        
+    </head>
+    <body>
+        <header>
+            
+            <div id="logo">
+                
+                <img src="../images/CG360.png" alt="CyberGuard360 logo">
+                
+            </div>
+            
+            <div id="sign_up">
+                
+                 <input type="button"
+                        onclick="window.location.href='../login_page/login_page.php';"
+                        value="Sign In">
+                
+            </div>
+            
+   
+            
+            <ul class="nav">
+                
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Clients</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+                
+               
+            
+        </header>
+        
+        <!-- creating landing page Hero -->
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>About CyberGuard360</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus molestias omnis ad voluptatibus beatae, sunt voluptatum eos sequi culpa? Sed fugiat cupiditate dolore ipsum odio eum debitis odit facilis ipsam.</p>
+                <br>
+                <div class="card">
+                    <div class="card-header">
+                        Helpful Links
+                      </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">PCI DSS Guidelines: <a href="https://docs-prv.pcisecuritystandards.org/PCI%20DSS/Standard/PCI-DSS-v4_0.pdf">https://docs-prv.pcisecuritystandards.org/PCI%20DSS/Standard/PCI-DSS-v4_0.pdf</a></li>
+                        </ul>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Link 2:</li>
+                        </ul>
+                </div>
+            </div>
+            
+        </div>
+        
+    </body>
+</html>
