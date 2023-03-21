28b752233ec9c4f89384f83c7fc9a38c11871f76
AwesomenessPrimal <brendanfisher88@gmail.com>
2023-03-21 09:25:15 -0400

    Revert "Swapped to webuser and Ip login"
    This reverts commit 86edb83b2c4d1294972b179dd6dfe82422970334.

--- model/database.php
+++ model/database.php
@@ -2,9 +2,9 @@
 
     // use 45.27.16.116
     //  Port: 3306
-    $dsn = 'mysql:host=45.27.16.116;dbname=cyberguard360';
-    $username = 'webuser';
-    $password = 'capstone2023';
+    $dsn = 'mysql:host=localhost;dbname=cyberguard360';
+    $username = 'root';
+    $password = '';
 
     try {
         $db = new PDO($dsn, $username, $password);
