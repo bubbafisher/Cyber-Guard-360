88224dc091ffc62009519b4ea66c7dd1199791cf
AwesomenessPrimal <brendanfisher88@gmail.com>
2023-03-29 10:16:50 -0400

    Added get_question to get the question info

--- quiz/index.php
+++ quiz/index.php
@@ -22,7 +22,7 @@ switch($action)
         break;
     case 'back':
         $survey_id = filter_input(INPUT_POST, 'survey_id');
-        $question = get_last_question(filter_input(INPUT_POST, 'survey_id'));
+        $question = get_question(get_last_question($survey_id));
         include('quiz.php');
     case 'next':
         if(filter_input(INPUT_POST, 'answer') != null)
