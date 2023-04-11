2233b2840d3072ae03e652073241c9dbe59de2b8
AwesomenessPrimal <brendanfisher88@gmail.com>
2023-04-11 09:15:22 -0400

    Added Contact Us Page

---
+++ contact/contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+              rel="stylesheet"
+              integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+              crossorigin="anonymous">
+        
+        
+        <link rel="stylesheet" href="../main.css"/>
+        <title>CyberGuard 360</title>
+        
+    </head>
+    <body>
+        <header>
+            
+            <div id="logo">
+                
+                 <a href="../index.php"><img
+                        src="../images/CG360.png" 
+                        alt="CyberGuard360 logo"></a>
+                
+            </div>
+            
+            <div id="sign_up">
+                
+                 <input type="button"
+                        onclick="window.location.href='../login_page/login_page.php';"
+                        value="Sign In">
+                
+            </div>
+            
+   
+            
+            <ul class="nav">
+                
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Clients</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+                
+               
+            
+        </header>
+        
+        <!-- creating landing page Hero -->
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>Contact Us</h1>
+                <ul>
+                    <li>Brendan Fisher - <a href="mailto:fisherbm@g.cofc.edu">fisherbm@g.cofc.edu</a></li>
+                    <li>Chase Friedfertig - <a href="mailto:friedfertigc@g.cofc.edu">friedfertigc@g.cofc.edu</a></li>
+                    <li>Zach Pace - <a href="mailto:pacezd@g.cofc.edu">pacezd@g.cofc.edu</a></li>
+                    <li>Christy Schoener - <a href="mailto:shoenerca@g.cofc.edu">shoenerca@g.cofc.edu</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        
+   <?php include '../views/footer.php';
